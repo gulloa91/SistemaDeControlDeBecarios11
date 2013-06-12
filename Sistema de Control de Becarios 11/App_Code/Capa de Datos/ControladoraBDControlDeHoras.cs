fcb753d833319acaa93a9abb28ef1b8b3361f2f1
Heriberto Ureña Madrigal <burenam17@hotmail.com>
2013-06-12 10:39:43 -0600

    Control de horas
    Controladora BD y DataSet

---
+++ Sistema de Control de Becarios 11/App_Code/Capa de Datos/ControladoraBDControlDeHoras.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ControlDeHorasDataSetTableAdapters;
+using System.Data;
+
+public class ControladoraBDControlDeHoras
+{
+    private ControlDeHorasTableAdapter adapterControlDeHoras;
+	public ControladoraBDControlDeHoras()
+	{
+        adapterControlDeHoras = new ControlDeHorasTableAdapter();
+	}
+
+    public DataTable consultarReportesBecarios(String cedulaEncargado, int estado){
+        DataTable dt = new DataTable();
+        dt = adapterControlDeHoras.consultarReportesBecarios(cedulaEncargado, estado);
+        return dt;
+    }
+
+    public DataTable consultarReportesHorasBecarios(String cedulaEncargado, String cedulaBecario, int estado) {
+        DataTable dt = new DataTable();
+        dt = adapterControlDeHoras.consultarReportesHorasBecarios(cedulaBecario, cedulaEncargado, estado);
+        return dt;
+    }
+}
