6f3f22589d8f0fa5f7e5ccef8822a411b81234f5
manhid <maNu2176>
2013-06-12 10:28:34 -0600

    Constantino - Corrección PopUp de espera

--- Sistema de Control de Becarios 11/Scripts/funcionesGlobales.js
+++ Sistema de Control de Becarios 11/Scripts/funcionesGlobales.js
@@ -6,18 +6,3 @@
 
 }
 
-/** Mensaje de información **/
-$(document).ready(function () {
-    $("#popUpEspera").dialog({
-        autoOpen: false,
-        modal: true,
-        width: 350,
-        height: 
-        open: function () {
-            $(".ui-widget-overlay.ui-front").css('z-index', '1099');
-        },
-        close: function () {
-            $(".ui-widget-overlay.ui-front").css('z-index', '150');
-        },
-    }).parent().css('z-index', '1100');
-});
