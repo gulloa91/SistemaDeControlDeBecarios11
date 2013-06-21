2c5feea9170e660449b339beb76a71cd6cfaddf9
Christopher Sánchez <chrisan1992@gmail.com>
2013-06-21 09:58:36 -0600

    Control de Horas - Becario

--- Sistema de Control de Becarios 11/App_Code/Capa Negocios/ControladoraControlBecario.cs
+++ Sistema de Control de Becarios 11/App_Code/Capa Negocios/ControladoraControlBecario.cs
@@ -18,12 +18,23 @@ public class ControladoraControlBecario
         cb = new ControladoraControlBecarioBD();
 	}
 
-    public DataTable horasReportadas(String becario) {
-        return cb.horasReportadas(becario);
+    public DataTable horasReportadas(String becario,String encargado) {
+        return cb.horasReportadas(becario,encargado);
     }
 
     //retorna la cedula del encargado para el becario en el periodo actual
     public String getCedulaEncargado(String becario) {
         return cb.getCedEncargado(becario);
     }
+
+    public String enviarReporte(Object[] datos)
+    {
+        ControlDeHoras c = new ControlDeHoras(datos);
+        return this.cb.enviarReporte(c);
+    }
+
+    public int modificarReporte(Object [] datos) {
+        return this.cb.modificarReporte(new ControlDeHoras(datos));
+    }
 }
+
