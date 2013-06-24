a1e631e961baec9f1871135cc3b2ae8577411ed8
Gabriel Ulloa <gulloa91@gmail.com>
2013-06-23 20:39:02 -0600

    Control de Horas - Faltaban

--- Sistema de Control de Becarios 11/Scripts/ControlDeHorasBecario.js
+++ Sistema de Control de Becarios 11/Scripts/ControlDeHorasBecario.js
@@ -1,7 +1,28 @@
 ﻿$(document).ready(function () {
-
     setPopUp("PopUpCtrlBecario", "btnInvisibleEnviarReporte");
-    //setDeletePopUp("PopUpEliminarAsignacion", "btnInvisible2");
     $("#PopUpCtrlBecario").dialog("option", "width", 600);
     $(".dateText").datepicker();
+
+    $("#siguienteAsig").dialog({
+        autoOpen: false,
+        modal: true,
+        width: 430,
+        appendTo: "form",
+        buttons: {
+            "Aceptar": function () {
+                $(".btnInvisibleAsig").click();
+            }
+
+        }
+    }).parent().css('z-index', '1010');
+
 });
+
+
+function abrir() {
+    $("#siguienteAsig").dialog({ title: "Asignacion" });
+    $("#siguienteAsig").dialog("open");
+}
+function cerrar() {
+    $("#siguienteAsig").dialog("close");
+}
--- Sistema de Control de Becarios 11/Scripts/ControlDeHorasEncargado.js
+++ Sistema de Control de Becarios 11/Scripts/ControlDeHorasEncargado.js
@@ -1,7 +1,58 @@
 ﻿$(document).ready(function () {
 
-    setPopUp("PopUpControlDeHorasEncargado", "btnInvisibleEnviarRevision");
+    setPopUpHorasBecario("PopUpControlDeHorasEncargado", "MainContent_btnInvisibleEnviarRevision", "MainContent_btnInvisibleCancelarRevision");
     //setDeletePopUp("PopUpEliminarAsignacion", "btnInvisible2");
     $("#PopUpControlDeHorasEncargado").dialog("option", "width", 600);
+
+    setPopUpAsignarBecario("popUpConfirmar", "MainContent_btnInvisibleAsignacion", "MainContent_btnInvisibleAsignacion2");
+    //setDeletePopUp("PopUpEliminarAsignacion", "btnInvisible2");
+    $("#popUpConfirmar").dialog("option", "width", 600);
 });
 
+
+/** Función que se encarga de crear un Pop Up para asignar nuevamente becario a encargado **/
+function setPopUpAsignarBecario(popUpId, aceptarClick, rechazarClick) {
+    $("#" + popUpId).dialog({
+        autoOpen: false,
+        modal: true,
+        appendTo: "form",
+        width: 500,
+        buttons: {
+            "Aceptar": function () {
+                //alert("Acepto");
+                //$(this).dialog("close");
+                $("#" + aceptarClick).click();
+            },
+
+            "Rechazar": function () {
+                $("#" + rechazarClick).click();
+                //alert("Rechazo");
+                //$(this).dialog("close");
+            }
+
+        }
+    }).parent().css('z-index', '1005');
+}
+
+function setPopUpHorasBecario(popUpId, aceptarClick, rechazarClick) {
+    $("#" + popUpId).dialog({
+        autoOpen: false,
+        modal: true,
+        appendTo: "form",
+        width: 500,
+        buttons: {
+            "Aceptar": function () {
+                //alert("Acepto");
+                //$(this).dialog("close");
+                $("#" + aceptarClick).click();
+            },
+
+            "Cancelar": function () {
+                $("#" + rechazarClick).click();
+                //alert("Rechazo");
+                //$(this).dialog("close");
+            }
+
+        }
+    }).parent().css('z-index', '1005');
+}
